7352dd1ad5af5ca2a95d022a9492dd24ea1fc927
Santiago0930 <santiago.guerrerod@javeriana.edu.co>
2025-03-28 21:57:44 -0500

    create components

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'notFound', component: NotFoundComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, 
+    {
+      bindToComponentInputs: true, // Para poder usar @Input en rutas https://angular.io/guide/router
+      onSameUrlNavigation: 'reload' // https://stackoverflow.com/a/52512361
+    })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
