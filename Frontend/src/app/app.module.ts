b5aa87ec04a7670e63802aa1857aa56cb4cb3d28
Luisa vargas <luchisvargasgomez@gmail.com>
2025-04-01 15:53:14 -0500

    Se agrego la conexión con el back

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -15,6 +20,9 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     NotFoundComponent
   ],
   imports: [
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  nombre: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {}
+
+  registrar() {
+    const nuevoUsuario = {
+      nombre: this.nombre,
+      email: this.email,
+      password: this.password,
+      rol: 'CLIENTE' // lo convierte al enum Rol.CLIENTE automáticamente
+    };
+
+    this.usuarioService.registrarUsuario(nuevoUsuario).subscribe({
+      next: (res) => {
+        console.log('✅ Usuario registrado:', res);
+        alert('¡Te has registrado exitosamente!');
+        this.router.navigate(['/home']); // 👈 redirección automática
+      },
+      error: (err) => {
+        console.error('❌ Error al registrar:', err);
+        alert('El correo ya está en uso o hubo un problema.');
+      }
+    });
+  }
 }
