af43b81bd161d3896a3da4db7a1f362724bea3ca
Santiago Barajas <santibabel@hotmail.com>
2025-04-12 17:28:10 -0500

    v2.0 Correxion carpetas

---
+++ src/app/components/shared/user-info-panel/user-info-panel.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserInfoPanelComponent } from './user-info-panel.component';
+
+describe('UserInfoPanelComponent', () => {
+  let component: UserInfoPanelComponent;
+  let fixture: ComponentFixture<UserInfoPanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserInfoPanelComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserInfoPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
