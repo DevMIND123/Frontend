73a91f2364a1bb323e409905234743dede64cdec
Santiago Barajas <santibabel@hotmail.com>
2025-04-12 16:58:36 -0500

    v2.0 Refactorizacion proyecto en angular

---
+++ Local Sites/Frontend/Frontend/src/app/components/shared/user-info-panel/user-info-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-panel',
+  standalone: false,
+  templateUrl: './user-info-panel.component.html',
+  styleUrl: './user-info-panel.component.css'
+})
+export class UserInfoPanelComponent {
+
+}
