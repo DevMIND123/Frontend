1df8dec19f891ac200dbacff51c17e579f0be886
Gustavo <gustavoparratanoux@gmail.com>
2025-05-06 23:37:11 -0500

    Quitar innecesario userId en request de notificaciones

--- src/app/services/notificaciones.service.ts
+++ src/app/services/notificaciones.service.ts
@@ -8,14 +8,16 @@ const NOTIF_URL = environment.apiUrl;
 
 @Injectable({ providedIn: 'root' })
 export class NotificacionesService {
+  constructor(private readonly http: HttpClient) {}
 
-    constructor(private readonly http: HttpClient) { }
+  getNotificacionesPorUsuario(idUsuario: number): Observable<any[]> {
+    return this.http.get<any[]>(`${NOTIF_URL}/notificaciones/`);
+  }
 
-    getNotificacionesPorUsuario(idUsuario: number): Observable<any[]> {
-        return this.http.get<any[]>(`${NOTIF_URL}/notificaciones/${idUsuario}`);
-    }
-
-    marcarComoLeida(idNotificacion: number): Observable<void> {
-        return this.http.patch<void>(`${NOTIF_URL}/notificaciones/${idNotificacion}/leida`, {});
-    }
+  marcarComoLeida(idNotificacion: number): Observable<void> {
+    return this.http.patch<void>(
+      `${NOTIF_URL}/notificaciones/${idNotificacion}/leida`,
+      {}
+    );
+  }
 }
