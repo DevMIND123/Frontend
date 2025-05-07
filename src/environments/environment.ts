cf32e9a0d732b82f7360386437870015045ae11a
jnicolas_moyano@javeriana.edu.co <jnicolas_moyano@javeriana.edu.co>
2025-05-07 11:47:20 -0500

    Add mocks Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://10.43.103.60:8000/api.retochimba.com'
+    apiUrl: 'http://10.43.103.60:8000/api.retochimba.com',
+    mocksUrl: 'http://10.43.103.60:8000'
   };
